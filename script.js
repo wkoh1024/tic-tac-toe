319cd1175eb09d1598e479ebbd979e9941645b92
Wayne <waynekoh1024@gmail.com>
2025-04-14 21:27:22 -0400

    Can now tell when there is win in column direction

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const Gameboard = (function() {
 
     // let boardState = [...Array(3)].map(e => Array(3).fill(0));
     let boardState = [
-        [-1, -1, -1],
+        [-1, 1, -1],
         [-1, 1, 0],
-        [0, -1, 1]
+        [-1, -1, 1]
       ]
 
     function playerOneWin() {
@@ -25,17 +25,30 @@ const Gameboard = (function() {
     function checkWin() {
         for (let i = 0; i < boardState.length; i++) {
             if (boardState[i]) {
-                if (calculateRowSum(i) == 3 || calculateRowSum(i) == -3) {
+                if (calcRowSum(i) == 3 || calcRowSum(i) == -3) {
+                    console.log("row win found");
                     return boardState[i][boardState.length - 1];
+                }
+                if (calcColumnSum(i) == 3 || calcColumnSum(i) == -3) {
+                    console.log("column win found");
+                    return boardState[boardState.length - 1][i];
                 }
             }
         }
         console.log("no win");
     };
 
-    function calculateRowSum (index) {
+    function calcRowSum (index) {
         let rowSum = boardState[index].reduce((sum, num) => sum + num);
         return rowSum;
+    }
+
+    function calcColumnSum (index) {
+        let columnSum = 0;
+        for (let i = 0; i < boardState.length; i++) {
+            columnSum += boardState[i][0];
+        }
+        return columnSum;
     }
 
     console.log(boardState);
