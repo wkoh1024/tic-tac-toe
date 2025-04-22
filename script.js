b1ccfd84c8f57c676cf171184db71ca57c380f9d
Wayne <waynekoh1024@gmail.com>
2025-04-21 22:35:33 -0400

    Added boardState rendering to DOM

--- script.js
+++ script.js
@@ -1,25 +1,17 @@
-const Gameboard = (function() {
-    let playerOneScore = 0;
-    let playerTwoScore = 0;
-    let playerOneTurn = true;
+// let boardState = [...Array(3)].map(e => Array(3).fill(0));
+let boardState = [
+    [1, 1, -1],
+    [1, -1, -1],
+    [-1, -1, 1]
+];
 
-    // let boardState = [...Array(3)].map(e => Array(3).fill(0));
-    let boardState = [
-        [-1, 1, 1],
-        [-1, 0, -1],
-        [-1, -1, 1]
-    ];
-
+const displayController = (function() {
     //cache DOm
     let $startButton = document.querySelector("#startButton button");
     let $startModal = document.querySelector("#startGame");
     let $submit = document.querySelector(`input[type="submit"`);
     let $main = document.querySelector("#main");
-
-    // bind events
-    $startButton.addEventListener("click", () => {
-        $startModal.showModal();
-    })
+    let $gridItems = document.querySelector(".gameboard").children;
 
     const submitButtonHandler = (event) => {
         event.preventDefault();
@@ -40,12 +32,39 @@ const Gameboard = (function() {
         $main.style.display = "block";
         $startModal.close();
         $startButton.style.display = "none";
-
-
     }
 
+    const render = () => {
+        for (let i = 0; i < boardState.length; i++) {
+            for (let j = 0; boardState[i].length; j++) {
+                if (boardState[i][j] == 1) {
+                    $gridItems[i + j].textContent = 'X';
+                }
+                else if (boardState[i][j] == -1) {
+                    $gridItems[i + j].textContent = 'O';
+                }
+            }
+        }
+        // for (let i = 0; i < $gridItems.length; i++) {
+        //     $gridItems[i].addEventListener("click", function () {
+        //         console.log($gridItems[i].id)
+        //     })
+        // }
+    }
+    
+    // bind events
+    $startButton.addEventListener("click", () => {
+        $startModal.showModal();
+    });
     $submit.addEventListener("click", submitButtonHandler);
+    render();
+    return {render};
+})();
 
+const Gameboard = (function() {
+    let playerOneScore = 0;
+    let playerTwoScore = 0;
+    let playerOneTurn = true;
 
     const getPlayerOneScore = () => playerOneScore;
     const getPlayerTwoScore = () => playerTwoScore;
@@ -57,6 +76,48 @@ const Gameboard = (function() {
     const changeTurn = () => playerOneTurn = !playerOneTurn;
     
     const checkWin = () => {
+        const calcRowSum = (index) => {
+            let rowSum = boardState[index].reduce((sum, num) => sum + num);
+            return rowSum;
+        }
+    
+        const calcColumnSum = (index) => {
+            let columnSum = 0;
+            for (let i = 0; i < boardState.length; i++) {
+                columnSum += boardState[i][index];
+            }
+            return columnSum;
+        }
+    
+        const diagonalVictory = () => {
+            let diagonalSum = 0;
+            let counterDiagonalSum = 0;
+            let arraySum = [];
+            for (let i = 0; i < boardState.length; i++) {
+                diagonalSum += boardState[i][i];
+                counterDiagonalSum += boardState[i][boardState.length - 1 - i];
+            }
+            arraySum.push(diagonalSum);
+            arraySum.push(counterDiagonalSum);
+            // logic needs to be explained
+            if (arraySum.includes(3)) {
+                return arraySum.indexOf(3);
+            }
+            else if (arraySum.includes(-3)) {
+                return arraySum.indexOf(-3);
+            }
+        }
+
+        const checkTie  = () => {
+            let tie = true;
+            for (let i = 0; i < boardState.length; i++) {
+                if (boardState[i].includes(0)) {
+                    tie = false;
+                }
+            }
+            return tie;
+        }
+
         for (let i = 0; i < boardState.length; i++) {
             if (calcRowSum(i) == 3 || calcRowSum(i) == -3) {
                 console.log("row win found");
@@ -81,50 +142,7 @@ const Gameboard = (function() {
         console.log("no win yet");
     };
 
-    const calcRowSum = (index) => {
-        let rowSum = boardState[index].reduce((sum, num) => sum + num);
-        return rowSum;
-    }
-
-    const calcColumnSum = (index) => {
-        let columnSum = 0;
-        for (let i = 0; i < boardState.length; i++) {
-            columnSum += boardState[i][index];
-        }
-        return columnSum;
-    }
-
-    const diagonalVictory = () => {
-        let diagonalSum = 0;
-        let counterDiagonalSum = 0;
-        let arraySum = [];
-        for (let i = 0; i < boardState.length; i++) {
-            diagonalSum += boardState[i][i];
-            counterDiagonalSum += boardState[i][boardState.length - 1 - i];
-        }
-        arraySum.push(diagonalSum);
-        arraySum.push(counterDiagonalSum);
-        // logic needs to be explained
-        if (arraySum.includes(3)) {
-            return arraySum.indexOf(3);
-        }
-        else if (arraySum.includes(-3)) {
-            return arraySum.indexOf(-3);
-        }
-    }
-
-    const checkTie  = () => {
-        let tie = true;
-        for (let i = 0; i < boardState.length; i++) {
-            if (boardState[i].includes(0)) {
-                tie = false;
-            }
-        }
-        return tie;
-    }
-
     console.log(boardState);
     console.log(checkWin());
-
-    return {checkWin};
+    return {playerOneTurn, checkWin}
 })();
